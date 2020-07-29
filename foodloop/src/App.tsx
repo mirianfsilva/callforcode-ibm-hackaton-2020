1c4e1eb7efc5b269d881cb5e51c3dffcc08aa636
mirianfsilva <mirian@ibm.com>
2020-07-29 00:25:41 -0300

    add editor config

--- foodloop/src/App.tsx
+++ foodloop/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Menu from './components/Menu';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Menu></Menu>
+            </div>
+        );
+    }
 }
 
 export default App;
--- foodloop/src/components/Menu/index.tsx
+++ foodloop/src/components/Menu/index.tsx
@@ -1,2 +1,23 @@
 import React from 'react';
+import logoMenu from '../../assets/images/foodloopmenulogo.png';
+import { render } from '@testing-library/react';
 
+function Menu() {
+	return (
+		<div className="navbar-fixed">
+			<nav>
+				<div className="nav-wrapper">
+					<img src={logoMenu} alt="Logo" />
+					<ul className="right hide-on-med-and-down">
+						<li><a href="/">HOME</a></li>
+						<li><a href="/">OUR PROPOSAL</a></li>
+                        <li><a href="/">PLANS</a></li>
+						<li><a href="/">CONTACT</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default Menu;
