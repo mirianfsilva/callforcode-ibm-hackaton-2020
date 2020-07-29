b8da7792fa8cbe8564d85f31795301e55c9c4a4f
mirianfsilva <mirian@ibm.com>
2020-07-29 00:00:39 -0300

    add components folders:

--- foodloop/src/App.tsx
+++ foodloop/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+
     </div>
   );
 }
