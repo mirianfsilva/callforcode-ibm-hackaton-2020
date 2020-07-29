214e23a1467a83a6c5ac5141584b9d78aaaab652
mirianfsilva <mirian@ibm.com>
2020-07-29 01:08:29 -0300

    deu ruim

--- foodloop/src/index.tsx
+++ foodloop/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.scss';
+import 'materialize-css/dist/js/materialize';
 
 ReactDOM.render(
   <React.StrictMode>
