6bb218cef31201bafffeb82cd5a97b29d312c9d1
mirianfsilva <mirian@ibm.com>
2020-07-30 03:08:20 -0300

    create cards

--- foodloop/src/components/Proposal/index.js
+++ foodloop/src/components/Proposal/index.js
@@ -1,21 +1,123 @@
 import React from 'react';
-import './style.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, Grid, CardMedia, CardContent, Typography } from '@material-ui/core';
+import image1 from './../../assets/img/img1.png';
+import image2 from './../../assets/img/img2.png';
+import image3 from './../../assets/img/img3.png';
 
-function OurProposal(props) {
+// Link a style sheet with a function component using the hook pattern.
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    },
+    media: {
+        height: 0,
+        paddingTop: '65.25%', // 16:9
+    },
+    
+}));
+/* para alinhar o rolê 
+.MuiGrid-spacing-xs-3 {
+    width: calc(100% + 24px);
+    /* margin: -12px; */
+//   justify-content: center;
+//}
+// 
+export default function OurProposal(props) {
+    const classes = useStyles();
+
     return (
-        <div class="row">
-            <div class="col s12 m6">
-                <div class="card">
-                    <div class="card-image">
-                        <img src={props.img} />
-                    </div>
-                    <div class="card-content">
-                        <p>{props.children}</p>
-                    </div>
-                </div>
-            </div>
+        <div className={classes.root}>
+            <Grid container spacing={3} justify="center">
+                <Grid item xs={6} sm={3}>
+                    <Card>
+                        <CardMedia
+                            className={classes.media}
+                            image={image1}
+                        />
+                        <CardContent>
+                            <Typography variant="inherit" color="initial" component="p">
+                                On sait depuis longtemps que travailler avec du texte lisible et
+                                contenant du sens est source de distractions.
+                      </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                    <Card>
+                        <CardMedia
+                            className={classes.media}
+                            image={image2}
+                        />
+                        <CardContent>
+                            <Typography variant="inherit" color="initial" component="p">
+                                On sait depuis longtemps que travailler avec du texte lisible et
+                                contenant du sens est source de distractions.
+                      </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                    <Card>
+                        <CardMedia
+                            className={classes.media}
+                            image={image3}
+                        />
+                        <CardContent>
+                            <Typography variant="inherit" color="initial" component="p">
+                                On sait depuis longtemps que travailler avec du texte lisible et
+                                contenant du sens est source de distractions.
+                      </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </div>
     );
+    /*<div className={classes.root}>
+        <Grid container spacing={1}>
+                <Card>
+                    <CardMedia
+                        className={classes.media}
+                        image={props.children}
+                    />
+                    <CardContent>
+                        <Typography variant="body" color="text" component="p">
+                            On sait depuis longtemps que travailler avec du texte lisible et
+                            contenant du sens est source de distractions.
+                        </Typography>
+                    </CardContent>
+                </Card>
+            
+            
+                <Card>
+                    <CardMedia
+                        className={classes.media}
+                        image={props.children}
+                    />
+                    <CardContent>
+                        <Typography variant="body" color="text" component="p">
+                            On sait depuis longtemps que travailler avec du texte lisible et
+                            contenant du sens est source de distractions.
+                        </Typography>
+                    </CardContent>
+                </Card>
+            
+                <Card>
+                    <CardMedia
+                        className={classes.media}
+                        image={props.children}
+                    />
+                    <CardContent>
+                        <Typography variant="body" color="text" component="p">
+                            On sait depuis longtemps que travailler avec du texte lisible et
+                            contenant du sens est source de distractions.
+                        </Typography>
+                    </CardContent>
+                </Card>
+            
+        </Grid>
+    </div>
+);*/
 }
 
-export default OurProposal;
--- foodloop/src/pages/Home/index.js
+++ foodloop/src/pages/Home/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import Menu from '../../components/Navbar'
 import FooterPage from '../../components/Footer';
 import OurProposal from '../../components/Proposal';
-import image1 from './../../assets/img/img1.png';
-import image2 from './../../assets/img/img2.png';
-import image3 from './../../assets/img/img3.png';
 
 function Home() {
     return (
         <div style={{ background: "#fcfdf4" }}>
             <Menu />
-            <OurProposal img={image2}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type.
-            </OurProposal>
-        
+            <OurProposal />
             <FooterPage />
         </div>
     );
