d773fdc6978444acdbcf702a03eb8eadf70bf3bb
mirianfsilva <mirian@ibm.com>
2020-07-30 20:01:23 -0300

    section plans update

--- foodloop/src/components/PlansSection/index.js
+++ foodloop/src/components/PlansSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, Hidden, Grid, CardMedia, CardContent, Typography, Container } from '@material-ui/core';
+import { Paper, Link, Card, Hidden, Grid, CardMedia, CardContent, Typography, Container } from '@material-ui/core';
 import image from './../../assets/img/img4.png';
 
 // Link a style sheet with a function component using the hook pattern.
@@ -25,6 +25,21 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         flexWrap: 'wrap',
     },
+    mainFeaturedPost: {
+        position: 'relative',
+        marginLeft: 80,
+        marginRight: 80,
+        color: theme.palette.common.white,
+        marginBottom: theme.spacing(2),
+        backgroundImage: 'url(image)',
+        // backgroundSize: 'cover',
+        // backgroundRepeat: 'no-repeat',
+        // backgroundPosition: 'center',
+    },
+    mainFeaturedPostContent: {
+        position: 'relative',
+        padding: theme.spacing(5),
+    },
 
 }));
 
@@ -32,19 +47,19 @@ export default function PlansSection(props) {
     const classes = useStyles();
 
     return (
-        <div className={classes.heroContent} style={{ backgroundColor: '#b5c76f' }}>
-            <Container maxWidth="lg">
-                <Grid container spacing={1} justify="center">
-                    <Hidden xsDown>
-                        <CardMedia className={classes.cardMedia} image={image} />
-                    </Hidden>
-                    <Grid item xs={12} sm={6}>
+        <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(${image})` }}>
+            {/* Increase the priority of the hero background image */}
+            {<img style={{ display: 'none' }} src={image} />}
+            {/* <div className={classes.overlay} /> */}
+            <Grid container justify="center">
+                <Grid item xs={12} sm={6}>
+                    <div className={classes.mainFeaturedPostContent}>
                         <Card>
                             <CardContent>
-                                <Typography component="h4" variant="inherit" align="center" color="initial" paragraph>
-                                OUR PROPOSAL
+                                <Typography component="h4" variant="inherit" align="center" color="initial" gutterBottom>
+                                    OUR PROPOSAL
                                 </Typography>
-                                <Typography variant="inherit" color="initial" component="p">
+                                <Typography variant="inherit" color="initial" paragraph>
                                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
                                     the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
                                     of type and scrambled it to make a type specimen book. It has survived not only five centuries,
@@ -52,12 +67,15 @@ export default function PlansSection(props) {
                                     in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
                                     with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                 </Typography>
+                                <Link variant="inherit" href="#">
+                                    link
+                                </Link>
                             </CardContent>
                         </Card>
-                    </Grid>
+                    </div>
                 </Grid>
-            </Container>
-        </div>
+            </Grid>
+        </Paper>
     );
 }
 
@@ -65,4 +83,5 @@ PlansSection.propTypes = {
     post: PropTypes.object,
 };
 
-{/*  */}
+{/*  */ }
+
