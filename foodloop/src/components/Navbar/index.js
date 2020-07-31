3080c1a3a32f3fd2dd15fff340d9f506b3babd93
mirianfsilva <mirian@ibm.com>
2020-07-31 17:08:37 -0300

    add link to menu items

--- foodloop/src/components/Navbar/index.js
+++ foodloop/src/components/Navbar/index.js
@@ -3,22 +3,39 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Button, CssBaseline, Typography, Container } from '@material-ui/core';
 import { List, ListItem, ListItemText } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
 import Logo from '../../assets/img/foodloop-menu-logo.png';
 import './style.scss';
 
+function ListItemLink(props) {
+    const { primary, to } = props;
+
+    const renderLink = React.useMemo(
+        () => React.forwardRef((itemProps, ref) => <RouterLink to={to} ref={ref} {...itemProps} />),
+        [to],
+    );
+
+    return (
+        <li>
+            <ListItem button component={renderLink}>
+                <ListItemText primary={primary} />
+            </ListItem>
+        </li>
+    );
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
     menuButton: {
-        marginRight: theme.spacing(6),
-        marginLeft: theme.spacing(6),
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2),
         color: "#536f4f",
     },
     menuList: {
-        marginRight: theme.spacing(6),
-        marginLeft: theme.spacing(6),
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2),
     },
     title: {
         flexGrow: 1,
@@ -33,15 +50,15 @@ export default function Menu(props) {
             <AppBar position="fixed">
                 <Toolbar position="fixed" style={{ backgroundColor: '#fff' }}>
                     <Typography variant="h6" className={classes.title}>
-                        <img className="Logo" src={Logo} alt="foodloop" />
+                        <a href="/"><img className="Logo" src={Logo} alt="foodloop"/></a>
                     </Typography>
                     <List variant="inherit" align="center" color="initial" className={classes.menuButton} justify="center">
                         <ListItem variant="inherit" align="center" color="initial" className={classes.menuList}>
-                            <ListItemText primary="HOME" />
-                            <ListItemText primary="OUR PROPOSAL" />
-                            <ListItemText primary="PLANS" />
-                            <ListItemText primary="ABOUT" />
-                            <ListItemText primary="CONTACT" />
+                            <ListItemLink primary="HOME" to="/" />
+                            <ListItemLink primary="OUR PROPOSAL" to="/" />
+                            <ListItemLink primary="PLANS" to="/" />
+                            <ListItemLink primary="ABOUT" to="/About" />
+                            <ListItemLink primary="CONTACT" to="/"/>
                         </ListItem>
                     </List>
                     <Button style={{ backgroundColor: '#749370' }} color="inherit">Login</Button>
